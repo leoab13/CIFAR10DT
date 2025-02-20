5325fa6995cf74ee8ffc28d3a8fdbd9dd9cbda28
leoab13 <l21090939@queretaro.tecnm.mx>
2025-02-20 12:19:37 -0600

    Ajuste de parametros 1

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from utils.trainer import *
 
 X_train, y_train, X_test, y_test = load_cifar10()
 X_train, X_test = normalize_images(X_train, X_test)
-train_and_evaluate(X_train, y_train, X_test, y_test, max_depth=10, min_samples_split=5)
+train_and_evaluate(X_train, y_train, X_test, y_test, max_depth=30, min_samples_split=5)
--- utils/model.py
+++ utils/model.py
@@ -1,5 +1,5 @@
 from utils.imports import *
 
-def get_decision_tree_model(max_depth=None, min_samples_split=2):
-    model = DecisionTreeClassifier(max_depth=max_depth, min_samples_split=min_samples_split)
+def get_decision_tree_model(max_depth=None, min_samples_split=2,criterion="entropy"):
+    model = DecisionTreeClassifier(max_depth=max_depth, min_samples_split=min_samples_split, criterion=criterion)
     return model
