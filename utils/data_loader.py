85bcafa39eea2b60062500d8fa1234bb237195a4
leoab13 <l21090939@queretaro.tecnm.mx>
2025-02-20 11:27:14 -0600

    data_loader updated

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,24 +1,24 @@
 from utils.imports import *
 
-DATA_PATH = "data/"  # Ruta donde descargaste el dataset
-
+DATA_PATH = "data/"
 
 def load_batch(file):
     with open(file, 'rb') as fo:
         batch = pickle.load(fo, encoding='bytes')
+
     X = batch[b'data'].reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
     y = np.array(batch[b'labels'])
     return X, y
 
-
 def load_cifar10():
-    X_train, y_train = [], []
+    x_train, y_train = [], []
     for i in range(1, 6):
         file = os.path.join(DATA_PATH, f"data_batch_{i}")
-        X, y = load_batch(file)
-        X_train.append(X)
+        x, y = load_batch(file)
+        x_train.append(x)
         y_train.append(y)
 
-    X_train = np.concatenate(X_train)
+    x_train = np.concatenate(x_train)
     y_train = np.concatenate(y_train)
-    X_test, y_test = load_batch(os.path.join(DATA_PATH, "test_batch"))
+    x_test, y_test = load_batch(os.path.join(DATA_PATH, "test_batch"))
+    return x_train, y_train, x_test, y_test
